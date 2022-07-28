d4a030e2a22472781a65180cf4d289593bd726e9
Winnie Wang <49203832+weijwang18@users.noreply.github.com>
2022-07-28 09:40:11 -0700

    Update Restaurant.cs

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -2,9 +2,15 @@ namespace BestRestaurants.Models
 {
   public class Restaurant
   {
+        public Restaurant()
+    {
+      this.Reviews = new HashSet<Review>();
+    }
+
     public int RestaurantId { get; set; }
     public string Description { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
+    public virtual ICollection<Review> Reviews { get; set; }
   }
 }
