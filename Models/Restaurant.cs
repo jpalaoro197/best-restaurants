9541724f8fca6b30fdc7ee0e5e5489b9689d1298
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-03 10:54:25 -0700

    begin refactoring in restaurant.cs

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,18 +1,15 @@
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
+using System;
 namespace BestRestaurants.Models
 {
   public class Restaurant
   {
-        public Restaurant()
-    {
-      this.Reviews = new HashSet<Review>();
-    }
-
     public int RestaurantId { get; set; }
     public string Description { get; set; }
-    public int CuisineId { get; set; }
-    public virtual Cuisine Cuisine { get; set; }
-    public virtual ICollection<Review> Reviews { get; set; }
+    [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime DateOpened { get; set; }
+    public virtual ICollection<CuisineRestaurant> JoinEntities { get;}
+
   }
 }
