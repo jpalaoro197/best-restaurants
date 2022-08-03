f4674a90f1f04a57a1bf85ccec621260f91d5f9e
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-03 14:33:20 -0700

    correct usages in database context file

--- Models/DesignTimeDbContext.cs
+++ Models/DesignTimeDbContext.cs
@@ -3,23 +3,23 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace UniversityRoster.Models
+namespace BestRestaurants.Models
 {
-  public class UniversityRosterContextFactory : IDesignTimeDbContextFactory<UniversityRosterContext>
+  public class BestRestaurantsContextFactory : IDesignTimeDbContextFactory<BestRestaurantsContext>
   {
 
-    UniversityRosterContext IDesignTimeDbContextFactory<UniversityRosterContext>.CreateDbContext(string[] args)
+    BestRestaurantsContext IDesignTimeDbContextFactory<BestRestaurantsContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<UniversityRosterContext>();
+      var builder = new DbContextOptionsBuilder<BestRestaurantsContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new UniversityRosterContext(builder.Options);
+      return new BestRestaurantsContext(builder.Options);
     }
   }
 }
