bfdc5aea3b3f251373b525780ac243a36cfdf86c
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-03 14:52:58 -0700

    correct bestrestaurantcontext  db set reference for cuisinerestaurant

--- Models/BestRestaurantsContext.cs
+++ Models/BestRestaurantsContext.cs
@@ -6,7 +6,7 @@ namespace BestRestaurants.Models
   {
     public DbSet<Cuisine> Cuisines { get; set; }
     public DbSet<Restaurant> Restaurants { get; set; }
-    public DbSet<CuisineRestaurant> CourseRestaurant { get; set; }
+    public DbSet<CuisineRestaurant> CuisineRestaurant { get; set; }
 
     public BestRestaurantsContext(DbContextOptions options) : base(options) { }
 
